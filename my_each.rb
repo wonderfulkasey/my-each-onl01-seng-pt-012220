424946807314be454d59183242f8e06aae4f8021
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-30 21:26:03 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,10 @@
 collection = [1, 2, 3, 4]
 
-  def my_each(collection) # put argument(s) here
+  def my_each(array) # put argument(s) here
   
-      puts "#{collection}"
-  # code here
-      yield
-      puts 
+  
   end
   
-  my_each(collection) do
-    puts 
+  my_each(array) do
+    
   end
