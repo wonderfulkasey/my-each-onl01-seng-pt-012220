f614357ea80fac42b648837a97a950b3f178abdc
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-30 21:29:33 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
-end
+
+
+def my_each(array) # put argument(s) here
+  i = 0 
+    while i < array.length 
+      yield array[i]
+       i += 1 
+    end
+  
+  array
+  end
