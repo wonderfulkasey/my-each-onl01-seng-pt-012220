dd8de0608d6f3e55b1314d2fc1c7fbc25d666f88
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-30 21:21:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 collection = [1, 2, 3, 4]
 
   def my_each(collection) # put argument(s) here
-  collection.each do |collection|
+  #collection.each do |collection|
   #while
   puts "#{collection}"
   # code here
